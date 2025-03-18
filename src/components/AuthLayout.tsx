af88d23840f2a1c13598139d24c0d1b92c9ca039
ROYKEEN\ejize <teerawat.tanatan@gmail.com>
2025-03-18 16:22:31 +0700

    Change ui and map menu

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -67,36 +67,6 @@ const AuthLayout = ({
 
                 {children}
 
-                <div className="flex items-center my-6">
-                  <div className="flex-auto mt-px border-t border-dashed border-gray-200 dark:border-slate-700"></div>
-                  <div className="mx-4 text-secondary">Or</div>
-                  <div className="flex-auto mt-px border-t border-dashed border-gray-200 dark:border-slate-700"></div>
-                </div>
-
-                {hasThirdPartyLogin &&
-                  <div className="flex gap-4 justify-center mb-6">
-                    <Link to="https://github.com/" target="_blank" className="btn border-light text-gray-400 dark:border-slate-700">
-                      <span className="flex justify-center items-center gap-2">
-                        <i className="mgc_github_line text-info text-xl"></i>
-                        <span className="lg:block hidden">Github</span>
-                      </span>
-                    </Link>
-                    <Link to="https://www.google.com/" className="btn border-light text-gray-400 dark:border-slate-700">
-                      <span className="flex justify-center items-center gap-2">
-                        <i className="mgc_google_line text-danger text-xl"></i>
-                        <span className="lg:block hidden">Google</span>
-                      </span>
-                    </Link>
-                    <Link to="https://www.facebook.com/" className="btn border-light text-gray-400 dark:border-slate-700">
-                      <span className="flex justify-center items-center gap-2">
-                        <i className="mgc_facebook_line text-primary text-xl"></i>
-                        <span className="lg:block hidden">Facebook</span>
-                      </span>
-                    </Link>
-                  </div>
-                }
-
-                {bottomLinks}
               </div>
             </div>
           </div>
---
+++ src/pages/jobtype-setting/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { records } from "../tables/data";
+import { Link } from 'react-router-dom';
+import { PageBreadcrumb } from '../../components';
+
+const JobType = () => {
+  return (
+    <>
+      <PageBreadcrumb title='Job Type' name='Job Type' breadCrumbItems={['Menu', 'Job Type']} />
+
+      <div className="card">
+        <div className="card-header">
+          <div className="flex justify-between items-center">
+            <h4 className="card-title">List</h4>
+          </div>
+        </div>
+        <div>
+          <div className="overflow-x-auto">
+            <div className="min-w-full inline-block align-middle">
+              <div className="overflow-hidden">
+                <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                  <thead>
+                    <tr>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Name</th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Age</th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Address</th>
+                      <th scope="col" className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+                    {(records || []).map((record, idx) => {
+                      return (
+                        <tr key={idx}>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">{record.name}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{record.age}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{record.address}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                            <Link className="text-primary hover:text-sky-700" to="#">Delete</Link>
+                          </td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default JobType
