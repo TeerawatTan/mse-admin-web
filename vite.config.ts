e250904b7fc0a2aa97ae83cdcacc0172e0cda1c0
Chaimongkon <yhai005@gmail.com>
2025-03-21 10:24:13 +0700

    JobType Fetch, Add, Edit, Delete

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
-  define: { 'process.env': {}, }
-})
+  plugins: [react()],
+  define: { "process.env": {} },
+  server: {
+    proxy: {
+      "/api/jobTypes": {
+        target: "https://bck.toat.co.th/api/webs/MasterJobType",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/jobTypes/, ""),
+      },
+      "/api/workPlaces": {
+        target: "https://bck.toat.co.th/api/webs/MasterWorkPlace",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/workPlaces/, ""),
+      },
+    },
+  },
+});
